63b23e024610f2d0553037e930151165f3445f15
Tanguri <jungwontwo2@naver.com>
2023-11-20 09:55:41 +0900

    3.회원가입 Post로 보내기

--- src/main/java/spring/membership/controller/MemberController.java
+++ src/main/java/spring/membership/controller/MemberController.java
@@ -2,6 +2,8 @@ package spring.membership.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MemberController {
@@ -9,5 +11,14 @@ public class MemberController {
     @GetMapping("/member/save")
     public String saveForm(){
         return "save";
+    }
+
+    @PostMapping("/member/save")
+    public String save(@RequestParam("memberEmail") String memberEmail,
+                       @RequestParam("memberPassword") String memberPassword,
+                       @RequestParam("memberName") String memberName) {
+        System.out.println("MemberController.save");
+        System.out.println("memberEmail = " + memberEmail);
+        return "index";
     }
 }
