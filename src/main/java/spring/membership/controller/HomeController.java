f578b6d9890d9f3fe0191162a563888aa0dd5d81
Tanguri <jungwontwo2@naver.com>
2023-11-20 09:39:43 +0900

    1. index.html 만들기

---
+++ src/main/java/spring/membership/controller/HomeController.java
@@ -0,0 +1,13 @@
+package spring.membership.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    //기본페이지 요청 메서드
+    @GetMapping("/")
+    public String index(){
+        return "index"; //=> templates 폴더 안에 index.html을 찾아가라~
+    }
+}
---
+++ src/test/java/spring/membership/MembershipApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.membership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
